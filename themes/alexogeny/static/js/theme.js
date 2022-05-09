e76ac2d26798647dfc4e3df24d19aa2a228ee8a1
alexogeny <6896115+alexogeny@users.noreply.github.com>
2022-05-09 21:03:08 +1000

    Add reading progress bar to desktop

--- themes/alexogeny/static/js/theme.js
+++ themes/alexogeny/static/js/theme.js
@@ -5,9 +5,18 @@ function toggleTheme () {
   setTheme(newTheme)
 }
 
-function setTheme(theme) {
+function setTheme (theme) {
   document.documentElement.setAttribute('data-theme', theme)
 }
 
 const theme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'dark'
 setTheme(theme)
+
+window.onload = function () {
+  const readBar = document.querySelector('#readBar')
+  const contentElement = document.querySelector('#readScroller')
+  contentElement.addEventListener('scroll', e => {
+    let width = contentElement.scrollTop / (contentElement.scrollHeight - contentElement.clientHeight) * 100
+    readBar.style.setProperty('width', `${ width }%`)
+  })
+}
